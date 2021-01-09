8e0d5f2c31b188b1f2df70bb891a678831f0626e
KishanMistri <14bit029@nirmauni.ac.in>
2021-01-09 17:04:40 +0530

    Added separate file for provider and setup creds

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,11 +1,4 @@
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "AKI********MWH"
-  secret_key = "DPw********raHNQ2"
-}
-
 resource "aws_instance" "FirstEC2"{
   ami = "ami-0a36eb8fadc976275" 
   instance_type = "t2.micro"
 }
-
