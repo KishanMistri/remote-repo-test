7101de8ce8e519f9f355551cbb1ed34522e093ee
KishanMistri <14bit029@nirmauni.ac.in>
2021-01-13 10:05:55 +0530

    updated tf file with minor variable update

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,4 +1,18 @@
+resource "aws_eip" "vm_eip"{
+   vpc = true
+}
+
+output "eip" {
+  value = aws_eip.vm_eip.public_ip
+}
+
 resource "aws_instance" "FirstEC2"{
   ami = "ami-0a36eb8fadc976275" 
   instance_type = "t2.micro"
+  //associate_public_ip_address = eip
 }
+
+output "instance"{
+  value = aws_instance.FirstEC2.
+}
+
