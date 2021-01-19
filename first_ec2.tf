86591e4a374fab4b596f09ffc788bd22c266e299
KishanMistri <14bit029@nirmauni.ac.in>
2021-01-19 23:41:07 +0530

    Adding more resouce provision in firstec2 file

--- first_ec2.tf
+++ first_ec2.tf
@@ -11,8 +11,27 @@ resource "aws_instance" "FirstEC2"{
   instance_type = "t2.micro"
   //associate_public_ip_address = eip
 }
-
 output "instance"{
-  value = aws_instance.FirstEC2.
+  value = aws_instance.FirstEC2.id
 }
 
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.FirstEC2.id
+  allocation_id = aws_eip.vm_eip.id
+}
+
+resource "aws_security_group" "ssh-access" {
+  name        = "KISHAN-SSH"
+  
+  ingress {
+    description = "Allowing SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_proxy_ip}/32"]
+  }
+
+  tags = {
+    Name = "SSH_ALLOWED"
+  }
+}
