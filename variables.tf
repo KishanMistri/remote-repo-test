29ed3382d08ec6c4cbaed2e22400517de24db559
KishanMistri <14bit029@nirmauni.ac.in>
2021-01-23 11:49:57 +0530

    updated Variables for elb

--- variables.tf
+++ variables.tf
@@ -3,5 +3,15 @@ variable "my_proxy_ip"{
 } 
 
 variable "instancetype"{
+    type = string
     default = "t2.micro"
 } 
+
+variable "az" {
+    type = list
+    default = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
+variable "elb_timeout" {
+    type = number
+    default = 300
+}
