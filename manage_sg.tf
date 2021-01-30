00105b2e864d3d1407cabc1bf0fbb5ab3deef7e3
KishanMistri <14bit029@nirmauni.ac.in>
2021-01-30 21:14:47 +0530

    Updated server configs and removed problematic configs

--- manage_sg.tf
+++ manage_sg.tf
@@ -13,12 +13,12 @@ resource "aws_security_group" "ssh_access" {
     Name = "SSH_ALLOWED"
   }
 }
-
+/*
 resource "aws_network_interface_sg_attachment" "ssh_sg_attachment" {
   security_group_id    = aws_security_group.ssh_access.id
   network_interface_id = aws_instance.web_server.primary_network_interface_id
 }
-
+*/
 resource "aws_security_group" "world_access" {
   name = "World-Access-Ports-SG"
   dynamic "ingress" {
@@ -35,8 +35,9 @@ resource "aws_security_group" "world_access" {
     Name = "Others"
   }
 }
-
+/*
 resource "aws_network_interface_sg_attachment" "web_sg_attachment" {
   security_group_id    = aws_security_group.world_access.id
   network_interface_id = aws_instance.web_server.primary_network_interface_id
 }
+*/
