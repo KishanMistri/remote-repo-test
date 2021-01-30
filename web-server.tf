2b18a8dad5f1e6e1d1bd70d4a371ba05af70bc69
KishanMistri <14bit029@nirmauni.ac.in>
2021-01-30 23:33:48 +0530

    Added SGs to Instance and private key hard coded for now. installing packages as well

--- web-server.tf
+++ web-server.tf
@@ -2,12 +2,16 @@ resource "aws_instance" "web_server" {
   ami             = var.region_ami[var.my_region]
   instance_type   = var.instance_type[var.my_region]
   key_name        = var.webserver_keypair
-  security_groups = [aws_security_group.ssh_access.id, aws_security_group.world_access.id]
+  vpc_security_group_ids = [aws_security_group.ssh_access.id, aws_security_group.world_access.id]
+  # security_groups = [aws_security_group.ssh_access.id, aws_security_group.world_access.id]
   #count = 1
 
   provisioner "remote-exec" {
     inline = [
-      "amazon-linux-extras install nginx1.12",
+      #"sudo su - && apt-get update && apt-get install -y lsb-release && apt-get clean all"
+      "sudo amazon-linux-extras install epel -y",
+      "sudo yum-config-manager --enable epel",
+      "sudo amazon-linux-extras install nginx1.12 -y",
       "sudo systemctl start nginx"
     ]
 
@@ -15,7 +19,7 @@ resource "aws_instance" "web_server" {
       type        = "ssh"
       host        = self.public_ip
       user        = "ec2-user"
-      private_key = file(var.webserver_keypair_localpath)
+      private_key = file("C:/Users/Kishan/.ssh/webserver_keypair.pem")
     }
   }
   tags = merge(
